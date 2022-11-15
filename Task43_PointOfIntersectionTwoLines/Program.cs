f15e068da7f0996db0551c8fb9ff6694a0c3d361
NataKuz <nv8282@mail.ru>
2022-11-15 21:38:14 +0800

    Changed solution for task 43

--- Task43_PointOfIntersectionTwoLines/Program.cs
+++ Task43_PointOfIntersectionTwoLines/Program.cs
@@ -17,19 +17,7 @@ double[,] GetTwoDimensionalArray(int countRows, int countColumns)
     return array;
 }
 
-void PrintTwoDimensionalArray(double[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-    }
-}
-
 double[,] matrix = GetTwoDimensionalArray(2, 2);
-PrintTwoDimensionalArray(matrix);
 
 if (matrix[0, 1] != matrix[1, 1])
 {
